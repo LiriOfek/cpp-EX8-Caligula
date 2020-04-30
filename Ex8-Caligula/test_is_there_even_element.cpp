04451808567fab4354f26cb5ef420b0b81056a9d
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 15:50:05 +0300

    add function that check if there is even element in vector boundaries

--- Ex8-Caligula/test_is_there_even_element.cpp
+++ Ex8-Caligula/test_is_there_even_element.cpp
@@ -7,6 +7,27 @@ Purpose: This file contain functions that test functions that check if number
 
 #include "test_is_there_even_element.h"
 
+void test_is_there_even_element_in_vector(std::vector<int> vec,
+										  int lowerBoundary,
+										  int upperBoundary) {
+	bool is_there_even_elemnt;
+
+	std::cout << ELEMENTS_IN_GIVEN_VECTOR << std::endl;
+	print_vec(vec);
+
+	is_there_even_elemnt = is_there_even_element_in_vector_boundaries(vec,
+		lowerBoundary,
+		upperBoundary);
+	std::cout << IS_THERE_EVEN_ELEMENT
+			  << lowerBoundary
+			  << COMMA
+			  << upperBoundary
+			  << QUESTION_MARK
+			  << TRUE_OR_FALSE[is_there_even_elemnt]
+			  << std::endl
+			  << std::endl;
+}
+
 void print_vec(std::vector<int> vec) {
 	for (int element : vec) {
 		std::cout << element << SPACE_BETWEEN_ELEMENTS;
--- Ex8-Caligula/test_is_there_even_element.h
+++ Ex8-Caligula/test_is_there_even_element.h
@@ -11,6 +11,26 @@ Purpose: This header file contain declaration of functions that test functions
 #include "is_there_even_element.h"
 
 const char* const SPACE_BETWEEN_ELEMENTS = " ";
+const char* const TRUE_OR_FALSE[] = { "False", "True" };
+const char* const ELEMENTS_IN_GIVEN_VECTOR = "The given vector: ";
+const char* const IS_THERE_EVEN_ELEMENT = "Is there even element in vector boundaries: ";
+const char* const COMMA = ", ";
+const char* const QUESTION_MARK = "? ";
+
+void test_is_there_even_element_in_vector(std::vector<int> vec,
+										  int lowerBoundary,
+										  int upperBoundary);
+/**
+* @brief  test the function that check if there is even element in the vector
+*			given boundaries and print the vector and if there is/isn't even
+*			element in the vector given boundaries.
+* @param  IN std::vector<int> vec - the given vector to check if there is
+*			even element in it given boundaries
+*		  IN int lowerBound - the given lower boundary of the vector
+*		  IN int upperBound - the given upper boundary of the vector
+* @return this function has no return value
+* @author  Liri
+*/
 
 void print_vec(std::vector<int> vec);
 /**
