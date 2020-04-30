22db3c63259981a6c7d4f11f3c8914e97178a4d5
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 10:55:53 +0300

    add main function with documentation

---
+++ Ex8-Caligula/main.cpp
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: main.cpp
+Author: Liri (28/4/2020)
+Purpose: This file initiate vectors and call to the function that test the
+			function that remove elements from the vector that equal to given 
+			number
+\********************************************************/
+
