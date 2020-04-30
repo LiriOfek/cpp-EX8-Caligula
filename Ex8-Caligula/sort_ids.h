a1419ab8cffb370f50d763127132dfe588f64c40
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 12:53:19 +0300

    add includes, constant and declaration of id_t

--- Ex8-Caligula/sort_ids.h
+++ Ex8-Caligula/sort_ids.h
@@ -4,3 +4,15 @@ Author: Liri (28/4/2020)
 Purpose: This header file contain declaration of functions that sort
 			vector of arrays in ascending order
 \********************************************************/
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <array>
+
+const int ID_LENGTH = 8;
+
+using id_t = std::array<char, ID_LENGTH>;
