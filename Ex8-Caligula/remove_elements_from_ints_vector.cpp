c5fa25214cf2c908cfd21de4b95325d0468bd484
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 10:38:16 +0300

    add cpp file and header file that remove elements that equal to given number from vector

---
+++ Ex8-Caligula/remove_elements_from_ints_vector.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: remove_elements_from_int_vector.cpp
+Author: Liri (28/4/2020)
+Purpose: This file contain functions that remove elements that
+			equal given value using algorithm
+\********************************************************/
---
+++ Ex8-Caligula/remove_elements_from_ints_vector.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: remove_elements_from_int_vector.h
+Author: Liri (28/4/2020)
+Purpose: This header file contain declaration of functions that remove
+			elements that equal than given value using
+			algorithm
+\********************************************************/
