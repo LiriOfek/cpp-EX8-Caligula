fa72995cef3644613e31d91bdfb9a3c89f721ca9
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 21:00:52 +0300

    add function that calculate n's element in fibonacci series

--- Ex8-Caligula/fibonacci_series.cpp
+++ Ex8-Caligula/fibonacci_series.cpp
@@ -5,3 +5,11 @@ Purpose: this file contain functions that calculate n's element in fibonacci
 			series and function that fill vector of fibonacci series in
 			given size
 \********************************************************/
+
+#include "fibonacci_series.h"
+
+int calculate_element_in_fibonacci_series(int n) {
+	const double sqrt5 = std::sqrt(5);
+	const double phi = (1 + sqrt5) / 2;
+	return (int)(std::pow(phi, n + 1) / sqrt5 + 0.5);
+}
--- Ex8-Caligula/fibonacci_series.h
+++ Ex8-Caligula/fibonacci_series.h
@@ -5,3 +5,20 @@ Purpose: this header file contain declaration of functions that
 			calculate n's element in fibonacci series and function that
 			fill vector of fibonacci series in given size
 \********************************************************/
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int calculate_element_in_fibonacci_series(int n);
+/**
+* @brief  calculate the n's element in fibonacci series according
+*			to formula to calculate n's element in fibonacci series
+* @param  IN int n - the number of the element in fiboncci series that
+*			need to be calculate
+*		  OUT the value of the n's element in fibonacci serius
+* @return the value of the n's element in fibonacci series
+* @author  Liri
+*/
