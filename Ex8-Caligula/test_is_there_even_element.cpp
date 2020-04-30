60476fa6906f7a366668bd8edb7a28230a455788
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 15:46:47 +0300

    add function that print given vector

--- Ex8-Caligula/test_is_there_even_element.cpp
+++ Ex8-Caligula/test_is_there_even_element.cpp
@@ -4,3 +4,12 @@ Author: Liri (28/4/2020)
 Purpose: This file contain functions that test functions that check if number
 			is even and if there is even number in given vector boundaries
 \********************************************************/
+
+#include "test_is_there_even_element.h"
+
+void print_vec(std::vector<int> vec) {
+	for (int element : vec) {
+		std::cout << element << SPACE_BETWEEN_ELEMENTS;
+	}
+	std::cout << std::endl;
+}
--- Ex8-Caligula/test_is_there_even_element.h
+++ Ex8-Caligula/test_is_there_even_element.h
@@ -5,3 +5,17 @@ Purpose: This header file contain declaration of functions that test functions
 			that check if number is even and if there is even number in
 			given vector boundaries
 \********************************************************/
+
+#pragma once
+
+#include "is_there_even_element.h"
+
+const char* const SPACE_BETWEEN_ELEMENTS = " ";
+
+void print_vec(std::vector<int> vec);
+/**
+* @brief  print the elements that in the given ints vector
+* @param  IN std::vector<int> ints - the vector of ints
+* @return this function has no return value
+* @author  Liri
+*/
