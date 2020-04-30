34bddb9a9b1bf44f3c902edaafbdd91230ead814
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 10:42:17 +0300

    add function that remove elements that equal to given number from vector and documentation

--- Ex8-Caligula/remove_elements_from_ints_vector.cpp
+++ Ex8-Caligula/remove_elements_from_ints_vector.cpp
@@ -4,3 +4,12 @@ Author: Liri (28/4/2020)
 Purpose: This file contain functions that remove elements that
 			equal given value using algorithm
 \********************************************************/
+
+#include "remove_elements_from_ints_vector.h"
+
+void remove_elements_equal_to_value(std::vector<int>&ints, int value) {
+	ints.erase(std::remove(ints.begin(),
+						   ints.end(),
+						   value),
+			   ints.end());
+}
--- Ex8-Caligula/remove_elements_from_ints_vector.h
+++ Ex8-Caligula/remove_elements_from_ints_vector.h
@@ -12,3 +12,18 @@ Purpose: This header file contain declaration of functions that remove
 #include <vector>
 #include <algorithm>
 #include <functional>
+
+void remove_elements_equal_to_value(std::vector<int>&ints, int value);
+/**
+* @brief  remove all elements from ints vector that equals to
+*			the given value
+* @param  INOUT std::vector<int>&ints - the vector of ints, that
+*				in the function changed such that all the elements that
+*				equals to value are removed
+*		   IN int value - the given value that all the elements that equals
+*			to it are removed from ints
+* @return this function has no return value
+* @notes  ints change in the function such that all the elements
+*			that equals to the given value are removed
+* @author  Liri
+*/
