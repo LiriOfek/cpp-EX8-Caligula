73111aa538d7ef8737566a96bd742457a93d916e
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 21:08:30 +0300

    add function that initiate fibonacci vector with elements equals to their indexes in the vector size

--- Ex8-Caligula/test_fibonacci_series.cpp
+++ Ex8-Caligula/test_fibonacci_series.cpp
@@ -8,9 +8,16 @@ Purpose: this file contain functions that test functions that calculate n's
 
 #include "test_fibonacci_series.h"
 
+void init_vector_with_elements_equals_to_indexes(std::vector<int>& fib) {
+	for (unsigned int index = 0; index < fib.size(); index++) {
+		fib[index] = index;
+	}
+}
+
 void print_fibonacci_vector(std::vector<int> fibonacci_vector) {
 	for (int element : fibonacci_vector) {
 		std::cout << element << SPACE_BETWEEN_ELEMENTS_IN_SERIES;
 	}
 	std::cout << std::endl;
 }
+
--- Ex8-Caligula/test_fibonacci_series.h
+++ Ex8-Caligula/test_fibonacci_series.h
@@ -13,6 +13,18 @@ Purpose: this header file contain declaration of functions that
 
 const char* const SPACE_BETWEEN_ELEMENTS_IN_SERIES = " ";
 
+void init_vector_with_elements_equals_to_indexes(std::vector<int>& fib);
+/**
+* @brief  initialize the given vector - that its elements will be equal
+*		   to their indexes
+* @param  IN std::vector<int>& fib - the vector of ints that will initate with
+*			the indexes of each element
+* @return this function has no return value
+* @notes  the given vector change in the function such that each element
+*			will be the index in the vector
+* @author  Liri
+*/
+
 void print_fibonacci_vector(std::vector<int> fibonacci_vector);
 /**
 * @brief  print the elements that in the given ints vector
