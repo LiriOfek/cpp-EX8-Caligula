cb204e672635141ab65e3a05063e78d74f6ca6a4
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 15:44:42 +0300

    add test files that test the function that check if there is even element in given vector boundaries

---
+++ Ex8-Caligula/test_is_there_even_element.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: test_is_there_even_element.cpp
+Author: Liri (28/4/2020)
+Purpose: This file contain functions that test functions that check if number
+			is even and if there is even number in given vector boundaries
+\********************************************************/
---
+++ Ex8-Caligula/test_is_there_even_element.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: test_is_there_even_element.h
+Author: Liri (28/4/2020)
+Purpose: This header file contain declaration of functions that test functions
+			that check if number is even and if there is even number in
+			given vector boundaries
+\********************************************************/
