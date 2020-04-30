d8bc45ba0dd6b91961f99d32913e3be07a8e1776
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 20:56:51 +0300

    add header file and cpp file that fill fibonacci vector

---
+++ Ex8-Caligula/fibonacci_series.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: fibonacci_series.cpp
+Author: Liri (28/4/2020)
+Purpose: this file contain functions that calculate n's element in fibonacci
+			series and function that fill vector of fibonacci series in
+			given size
+\********************************************************/
