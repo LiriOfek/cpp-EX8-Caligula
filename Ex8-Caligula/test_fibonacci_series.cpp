a10046149e99b55708f00d87720ab6aa86d13514
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 21:04:55 +0300

    add header and cpp files that test functions that create fibonacci vector

---
+++ Ex8-Caligula/test_fibonacci_series.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: test_fibonacci_series.cpp
+Author: Liri (28/4/2020)
+Purpose: this file contain functions that test functions that calculate n's
+			element in fibonacci series and function that
+			fill vector of fibonacci series in given size
+\********************************************************/
---
+++ Ex8-Caligula/test_fibonacci_series.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: fibonacci_series.h
+Author: Liri (28/4/2020)
+Purpose: this header file contain declaration of functions that
+			calculate n's element in fibonacci series and function that
+			fill vector of fibonacci series in given size
+\********************************************************/
