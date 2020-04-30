9c359c7102400f753ba07a3f680d65dfea71157e
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 12:54:44 +0300

    add function that sort ids vector in ascending order

--- Ex8-Caligula/sort_ids.cpp
+++ Ex8-Caligula/sort_ids.cpp
@@ -4,3 +4,9 @@ Author: Liri (28/4/2020)
 Purpose: This file contain functions that sort vector of arrays
 			in ascending order
 \********************************************************/
+
+#include "sort_ids.h"
+
+void ascending_sort_ids_vector(std::vector<id_t> &ids) {
+	std::sort(ids.begin(), ids.end());
+}
