680cb51e224bcae06a40fac62be58b3966ce6111
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 15:40:15 +0300

    add function that check if the given element is even

--- Ex8-Caligula/is_there_even_element.cpp
+++ Ex8-Caligula/is_there_even_element.cpp
@@ -4,3 +4,9 @@ Author: Liri (28/4/2020)
 Purpose: This file contain functions that check if number is even
 			and if there is even number in given vector boundaries
 \********************************************************/
+
+#include "is_there_even_element.h"
+
+bool even(int element) {
+	return (element % EVEN == 0);
+}
--- Ex8-Caligula/is_there_even_element.h
+++ Ex8-Caligula/is_there_even_element.h
@@ -5,3 +5,22 @@ Purpose: This header file contain declaration of functions
 			that check if number is even and if there is even number in
 			given vector boundaries
 \********************************************************/
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+const int EVEN = 2;
+
+bool even(int element);
+/**
+* @brief  check if the given element is even
+* @param  IN int element - the given element, to check if is even
+*		   OUT bool true - if the elemnt is even,
+*					false - otherwise
+* @return true - if the element is even,
+*		   false - otherwise
+* @author  Liri
+*/
