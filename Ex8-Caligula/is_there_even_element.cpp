c24c7b91f876c957affc858acf7d1a8c08160b9c
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 15:38:45 +0300

    add cpp and header files that check if there is even element in vactor given boundaries

---
+++ Ex8-Caligula/is_there_even_element.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: is_there_even_element.cpp
+Author: Liri (28/4/2020)
+Purpose: This file contain functions that check if number is even
+			and if there is even number in given vector boundaries
+\********************************************************/
---
+++ Ex8-Caligula/is_there_even_element.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: is_there_even_element.h
+Author: Liri (28/4/2020)
+Purpose: This header file contain declaration of functions
+			that check if number is even and if there is even number in
+			given vector boundaries
+\********************************************************/
