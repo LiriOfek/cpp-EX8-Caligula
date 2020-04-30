ebbf5ba79333db2d3a1510a42aa532b61f4836bf
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 12:52:04 +0300

    add cpp and header files of sort ids, with documentation

---
+++ Ex8-Caligula/sort_ids.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: sort_ids.cpp
+Author: Liri (28/4/2020)
+Purpose: This file contain functions that sort vector of arrays
+			in ascending order
+\********************************************************/
---
+++ Ex8-Caligula/sort_ids.h
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: sort_ids.h
+Author: Liri (28/4/2020)
+Purpose: This header file contain declaration of functions that sort
+			vector of arrays in ascending order
+\********************************************************/
