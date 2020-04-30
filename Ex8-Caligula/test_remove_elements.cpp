21f798037614075d3f7a723e58600eb6896b7944
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-30 10:47:28 +0300

    add function that print elements in given vector

--- Ex8-Caligula/test_remove_elements.cpp
+++ Ex8-Caligula/test_remove_elements.cpp
@@ -4,3 +4,12 @@ Author: Liri (28/4/2020)
 Purpose: This file contain functions that test the functions that remove
 			elements that equal to given value
 \********************************************************/
+
+#include "test_remove_elements.h"
+
+void print_ints_vector(std::vector<int> ints) {
+	for (int element : ints) {
+		std::cout << element << SPACE;
+	}
+	std::cout << std::endl;
+}
--- Ex8-Caligula/test_remove_elements.h
+++ Ex8-Caligula/test_remove_elements.h
@@ -5,3 +5,17 @@ Purpose: This header file contain declaration of functions that test the
 			functions that remove elements that equal to given
 			value
 \********************************************************/
+
+#pragma once
+
+#include "remove_elements_from_ints_vector.h"
+
+const char* const SPACE = " ";
+
+void print_ints_vector(std::vector<int> ints);
+/**
+* @brief  print the elements that in the given ints vector
+* @param  IN std::vector<int> ints - the vector of ints
+* @return this function has no return value
+* @author  Liri
+*/
